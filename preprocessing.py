ed428efb66f1540f9b26b2940d39c2fa361a1f88
chellycheng <hehuimin.cheng@mail.mcgill.ca>
2019-11-27 16:01:09 -0500

    lol

---
+++ preprocessing.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def tokenize_corpus(corpus):
+    tokens = [x.split() for x in corpus]
+    tokenized_corpus = tokenize_corpus(corpus)
+    vocabulary = []
+    for sentence in tokenized_corpus:
+        for token in sentence:
+            if token not in vocabulary:
+                vocabulary.append(token)
+
+    word2idx = {w: idx for (idx, w) in enumerate(vocabulary)}
+    idx2word = {idx: w for (idx, w) in enumerate(vocabulary)}
+    vocabulary_size = len(vocabulary)
+
+    window_size = 2
+    idx_pairs = []
+    # for each sentence
+    for sentence in tokenized_corpus:
+        indices = [word2idx[word] for word in sentence]
+        # for each word, threated as center word
+        for center_word_pos in range(len(indices)):
+            # for each window position
+            for w in range(-window_size, window_size + 1):
+                context_word_pos = center_word_pos + w
+                # make soure not jump out sentence
+                if context_word_pos < 0 or context_word_pos >= len(indices) or center_word_pos == context_word_pos:
+                    continue
+                context_word_idx = indices[context_word_pos]
+                idx_pairs.append((indices[center_word_pos], context_word_idx))
+
+    idx_pairs = np.array(idx_pairs)  # it will be useful to have this as numpy array
+    return idx_pairs
